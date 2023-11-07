b0079f0fd8983752116d1b74c76d89d02513f2de
Bao <20520142@gm.uit.edu.vn>
2023-11-07 23:02:10 +0700

    update model

--- app.py
+++ app.py
@@ -7,41 +7,41 @@ from flask import Flask, request, app, jsonify, url_for, render_template
 from sentence_transformers import SentenceTransformer
 # import re
 from preprocessing_data import *
-from deep_translator import GoogleTranslator
-
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 
 app = Flask(__name__, template_folder='template')
 
-our_model=pickle.load(open('models/predict_model/logistic_embedding_transformer_data_clean.pkl','rb'))
-model=SentenceTransformer('paraphrase-MiniLM-L6-v2')
+our_model=pickle.load(open('models/predict_model/svm_tf_data_clean.pkl','rb'))
+# model=SentenceTransformer('paraphrase-MiniLM-L6-v2')
+tf_vectorizer=pickle.load(open('models/tf_vec/tf_vect2_v2.pkl','rb'))
+
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.get_json()['data']
-    # print(data)
-    # data = GoogleTranslator(source='auto', target='en').translate(data)
     data=clean_data(str(data))
-    data_encode=model.encode(data)
-    # print(data_encode.reshape(1,-1))
+    data_encode=tf_vectorizer.transform([data])
     output=our_model.predict(data_encode.reshape(1,-1))
-    # print(int(output[0]))
     name_class=class_name(int(output[0]))
     return jsonify(data,name_class)
 
 @app.route('/predict',methods=['POST'])
 def predict():
     data= [x for x in request.form.values()]
-    data=clean_data(str(data[0]))
-    data_encode=model.encode(data)
+    data_clean=clean_data(str(data[0]))
+    data_encode=tf_vectorizer.transform([data_clean])
+    # data_encode=count_vectorizer.transform(data)
     output=our_model.predict(data_encode.reshape(1,-1))
     # print(int(output[0]))
     name_class=class_name(int(output[0]))
-    return render_template("index.html",prediction_text="Your emotion right now is {}".format(name_class))
+    return render_template("index2.html",
+                           prediction_text="Your emotion right now is {}".format(name_class),
+                           text_input=str(data[0]))
 
 
 if __name__=="__main__":
--- preprocessing_data.py
+++ preprocessing_data.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import re
 from deep_translator import GoogleTranslator
+import nltk
+nltk.download('wordnet')
+nltk.download('punkt')
+from nltk.stem import SnowballStemmer, LancasterStemmer, PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize, WhitespaceTokenizer
 # import contextualSpellCheck
 # import spacy
 
@@ -12,10 +17,46 @@ from deep_translator import GoogleTranslator
 # def translate_to_En(sentence):
 #     translated = GoogleTranslator(source='auto', target='en').translate(sentence)
 #     return translated
+#decontracted function
 def decontracted(phrase):
     # specific
     phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"wont", "will not", phrase)
+    phrase = re.sub(r"won\ t", "will not", phrase)
+
     phrase = re.sub(r"can\'t", "can not", phrase)
+    phrase = re.sub(r"cant", "can not", phrase)
+    phrase = re.sub(r"can\ t", "can not", phrase)
+
+    phrase = re.sub(r"didt", "did not", phrase)
+    phrase = re.sub(r"didnt", "did not", phrase)
+    phrase = re.sub(r"didn\ t", "did not", phrase)
+
+    phrase = re.sub(r"dont", "do not", phrase)
+    phrase = re.sub(r"don\ t", "do not", phrase)
+
+    phrase = re.sub(r"doest", "does not", phrase)
+    phrase = re.sub(r"doesnt", "does not", phrase)
+    phrase = re.sub(r"doesn\ t", "does not", phrase)
+
+    phrase = re.sub(r"isnt", "is not", phrase)
+    phrase = re.sub(r"isn\ t", "is not", phrase)
+
+    phrase = re.sub(r"arent", "are not", phrase)
+    phrase = re.sub(r"aren\ t", "are not", phrase)
+
+    phrase = re.sub(r"weve", "we have", phrase)
+    phrase = re.sub(r"we ve", "we have", phrase)
+    phrase = re.sub(r"we re", "we are", phrase)
+    phrase = re.sub(r"i m","i am",phrase)
+    phrase = re.sub(r"it s","it is",phrase)
+    phrase = re.sub(r"he s","he is",phrase)
+    phrase = re.sub(r"she s","she is",phrase)
+
+    phrase = re.sub(r"i ve","i have",phrase)
+    phrase = re.sub(r"they re","they are",phrase)
+    phrase = re.sub(r"they ve","they have",phrase)
+
 
     # general
     phrase = re.sub(r"n\'t", " not", phrase)
@@ -27,7 +68,102 @@ def decontracted(phrase):
     phrase = re.sub(r"\'ve", " have", phrase)
     phrase = re.sub(r"\'m", " am", phrase)
     return phrase
+slang_abbrev_dict = {
+    'AFAIK': 'As Far As I Know',
+    'AFK': 'Away From Keyboard',
+    'ASAP': 'As Soon As Possible',
+    'ATK': 'At The Keyboard',
+    'ATM': 'At The Moment',
+    'A3': 'Anytime, Anywhere, Anyplace',
+    'BAK': 'Back At Keyboard',
+    'BBL': 'Be Back Later',
+    'BBS': 'Be Back Soon',
+    'BFN': 'Bye For Now',
+    'B4N': 'Bye For Now',
+    'BRB': 'Be Right Back',
+    'BRT': 'Be Right There',
+    'BTW': 'By The Way',
+    'B4': 'Before',
+    'B4N': 'Bye For Now',
+    'CU': 'See You',
+    'CUL8R': 'See You Later',
+    'CYA': 'See You',
+    'FAQ': 'Frequently Asked Questions',
+    'FC': 'Fingers Crossed',
+    'FWIW': 'For What It\'s Worth',
+    'FYI': 'For Your Information',
+    'GAL': 'Get A Life',
+    'GG': 'Good Game',
+    'GN': 'Good Night',
+    'GMTA': 'Great Minds Think Alike',
+    'GR8': 'Great!',
+    'G9': 'Genius',
+    'IC': 'I See',
+    'ICQ': 'I Seek you',
+    'ILU': 'I Love You',
+    'IMHO': 'In My Humble Opinion',
+    'IMO': 'In My Opinion',
+    'IOW': 'In Other Words',
+    'IRL': 'In Real Life',
+    'KISS': 'Keep It Simple, Stupid',
+    'LDR': 'Long Distance Relationship',
+    'LMAO': 'Laugh My Ass Off',
+    'LOL': 'Laughing Out Loud',
+    'LTNS': 'Long Time No See',
+    'L8R': 'Later',
+    'MTE': 'My Thoughts Exactly',
+    'M8': 'Mate',
+    'NRN': 'No Reply Necessary',
+    'OIC': 'Oh I See',
+    'OMG': 'Oh My God',
+    'PITA': 'Pain In The Ass',
+    'PRT': 'Party',
+    'PRW': 'Parents Are Watching',
+    'QPSA?': 'Que Pasa?',
+    'ROFL': 'Rolling On The Floor Laughing',
+    'ROFLOL': 'Rolling On The Floor Laughing Out Loud',
+    'ROTFLMAO': 'Rolling On The Floor Laughing My Ass Off',
+    'SK8': 'Skate',
+    'STATS': 'Your sex and age',
+    'ASL': 'Age, Sex, Location',
+    'THX': 'Thank You',
+    'TTFN': 'Ta-Ta For Now!',
+    'TTYL': 'Talk To You Later',
+    'U': 'You',
+    'U2': 'You Too',
+    'U4E': 'Yours For Ever',
+    'WB': 'Welcome Back',
+    'WTF': 'What The Fuck',
+    'WTG': 'Way To Go!',
+    'WUF': 'Where Are You From?',
+    'W8': 'Wait',
+    '7K': 'Sick:-D Laugher'
+}
+def check_unslang(sentence):
+    """Converts text like "OMG" into "Oh my God"
+    """
+    words = word_tokenize(sentence)
+    all_words=[]
+    for word in words:
+      if word.upper() in slang_abbrev_dict.keys():
+          all_words.append(slang_abbrev_dict[word.upper()])
+      else:
+          all_words.append(word)
+    return " ".join(all_words)
 
+white_tokenizer = WhitespaceTokenizer() #word in space
+snow_stemmer=SnowballStemmer(language='english')
+lemmatizer = WordNetLemmatizer() #wlemmatizer word
+
+def lemmatize_stemming(sentence):
+    text = sentence
+    # text=' '.join([snow_stemmer.stem(w) for w in word_tokenize(str(sentence))])
+    text=' '.join([lemmatizer.lemmatize(w) for w in white_tokenizer.tokenize(str(text))])
+    return text
+def preprocess_stemmer(sentence):
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','', sentence['data_clean']) #clean url
+    text = lemmatize_stemming(str(text))
+    return text
 #General
 def clean_data(sentence):
     # sentence = MyMemoryTranslator(source='vi-VN', target='en-US').translate(sentence)
@@ -35,6 +171,7 @@ def clean_data(sentence):
     sentence = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','', sentence) #clean url
     sentence = re.sub('@[^\s]+','', sentence) #clean user
     sentence = sentence.lower() #low text
+    sentence = check_unslang(str(sentence))
     # doc=nlp(sentence)
     # sentence=doc._.outcome_spellCheck if doc._.performed_spellCheck else sentence
     sentence = decontracted(sentence)
@@ -42,7 +179,9 @@ def clean_data(sentence):
     sentence = re.sub('&[^\s]+;', '', sentence) #xóa html bắt đầu bằng &
     sentence = re.sub('[^a-zA-Za-яА-Я1-9]+', ' ', sentence) #xóa tất cả các lại dấu kí hiệu
     sentence = re.sub(' +',' ', sentence) # xóa câu chữ nhấn nhiều space
+    # sentence = lemmatize_stemming(str(sentence))
     return sentence
+
 def class_name(res):
     if res == 0:
         res="sadness"
